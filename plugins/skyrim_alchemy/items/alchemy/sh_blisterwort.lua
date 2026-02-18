81bd659c771bfd7f1d4e03b9c84466cf20fd2982
goyimm123 <bobaksobak@gmail.com>
2026-02-18 20:08:01 +0600

    Fix alchemy item bases and BrewPotion argument types

---
+++ plugins/skyrim_alchemy/items/alchemy/sh_blisterwort.lua
@@ -0,0 +1,9 @@
+ITEM.base = "base_alchemy"
+ITEM.name = "Blisterwort"
+ITEM.description = "A hot cave mushroom with a bitter smell."
+ITEM.model = Model("models/props_junk/garbage_milkcarton002a.mdl")
+ITEM.price = 24
+ITEM.alchemyEffects = {
+	"restore_health",
+	"fortify_stamina"
+}
---
+++ plugins/skyrim_alchemy/items/alchemy/sh_wheat.lua
@@ -0,0 +1,9 @@
+ITEM.base = "base_alchemy"
+ITEM.name = "Wheat"
+ITEM.description = "Dried grain that stabilizes medicinal mixtures."
+ITEM.model = Model("models/props_junk/garbage_bag001a.mdl")
+ITEM.price = 20
+ITEM.alchemyEffects = {
+	"restore_health",
+	"damage_health"
+}
